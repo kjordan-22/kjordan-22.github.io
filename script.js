e0bcb07c85a3ea978435f740c454629238f443cf
kjordan-22 <113301700+kjordan-22@users.noreply.github.com>
2022-12-23 15:46:41 -0500

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    function showModal(){
+        $('#modal-container').show();
+        $('html body').css('overflow','hidden');
+    }
+
+    function closeModal(){
+        $('#modal-container').hide();
+    }
+
+    setTimeout(showModal,3000);
+
+    $('#close').click(function(){
+        closeModal()
+    })
+})
